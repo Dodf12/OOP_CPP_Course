235a5984a940de1fde283f04e38d48480b81d8af
Deepika Dasari <deepikadasari@Deepikas-MacBook-Pro.local>
2024-09-28 18:51:02 -0700

    testing 123

---
+++ Week1/HW/helloworld.cpp
@@ -0,0 +1,39 @@
+//Abhinav Pala 9-28-24
+//I know the filepath of where I keep my work. Compiled and ran with no bugs
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	//First Paragrah
+	cout << endl;
+	cout << "\tHi Dr. Krehbiel! My name is Abhinav "
+	<<"I'm from San Jose, California. I am a freshman "
+	<<"I like playing basketball, light video games "
+	<< " and biking long distances." << endl << endl;
+
+	//Second Paragraph
+	cout << "\tI have coded starting in high school "
+	<< "so for about 4 years. I prevously learnt "
+	<< "python, and Java. I learned JavaScript "
+	<< "to a lower level and reviewd C++ basics "
+	<< "just this past summer. I am most "
+	<< "experienced in python and java, 4 years each "
+	<< "My main strengh is finding a solution, and "
+	<< "solving the problem. My biggest weakness is "
+	<<"debugging in general. To do well in CSCI 60, "
+	<< "I plan on studying early, revising daily, and "
+	<< "attending office hours. I also plan on using "
+	<<"services like stack overflow and reddit to address "
+	<<"bugs. I also have some friends in teh class that I "
+	<<"plan to study with. I also want to try experimenting "
+	<<"things I learned in class in code. Like if I learn "
+	<<"somethign in class, I want to test it out and explore "
+	<<"using the compiler to gain hands-on experience, rather "
+	<<"than just learning it on paper.";
+
+
+	return 0;
+}
--- Week1/intro.cpp
+++ Week1/intro.cpp
@@ -1,64 +1,100 @@
 #include <iostream>
 #include <string>
 #include <typeinfo>
+#include <fstream>
+
+bool isPrime(int n);
 
 int main()
 {
-  // use numbers without variables
+  // // use numbers without variables
 
-  std::cout << (5 + 3) << std::endl;
+  // std::cout << (5 + 3) << std::endl;
 
-  /*
-  DECLARING VARIABLES!!!
-  */
+  // /*
+  // DECLARING VARIABLES!!!
+  // */
 
-  double a = 5;
-  int b = 3;
+  // double a = 5;
+  // int b = 3;
 
-  auto c = a + b;
+  // double c = a + b;
 
-  std::cout << (a + b) << std::endl; // gives an int
-  std::cout << (a / b) << std::endl; // gives a float
+  // std::cout << (a + b) << std::endl; // gives an int
+  // std::cout << (a / b) << std::endl; // gives a float
 
-  std::cout << c;
+  // std::cout << c;
 
-  // Acceptign output from user
+  // // Acceptign output from user
 
-  int courseNumber;
-  std::cout << "Please enter the course number below";
-  std::cin >> courseNumber;
-  std::cout << "Welcome to CSCI " << courseNumber << "!" << std::endl;
+  // int courseNumber;
+  // std::cout << "Please enter the course number below";
+  // std::cin >> courseNumber;
+  // std::cout << "Welcome to CSCI " << courseNumber << "!" << std::endl;
 
-  // Array stuff
+  // // Array stuff
 
-  int threeInts[3] = {4, 6, 7};
-  std::cout << threeInts[0] << std::endl;
-  std::cout << threeInts[1] << std::endl;
-  std::cout << threeInts[2] << std::endl;
+  // int threeInts[3] = {4, 6, 7};
+  // std::cout << threeInts[0] << std::endl;
+  // std::cout << threeInts[1] << std::endl;
+  // std::cout << threeInts[2] << std::endl;
 
-  // Boolean
-  bool eligible = false;
-  std::cout << eligible << std::endl;
+  // // Boolean
+  // bool eligible = false;
+  // std::cout << eligible << std::endl;
 
-  eligible = true;
-  std::cout << eligible << std::endl;
+  // eligible = true;
+  // std::cout << eligible << std::endl;
 
-  std::cout << "You are " << (eligible ? "" : "not") << "eligible";
+  // std::cout << "You are " << (eligible ? "" : "not") << "eligible";
 
-  bool fax = true;
+  // bool fax = true;
 
-  std::cout << "It is " << (fax ? "" : "not") << "fax" << std::endl;
+  // std::cout << "It is " << (fax ? "" : "not") << "fax" << std::endl;
 
-  fax = false;
+  // fax = false;
 
-  std::cout << "It is " << (fax ? "" : "not") << "fax" << std::endl;
+  // std::cout << "It is " << (fax ? "" : "not") << "fax" << std::endl;
 
-  // 3 LOOPS
+  // // 3 LOOPS
 
-  std::cout << "Hip hip hooray!\n";
+  // std::cout << "Hip hip hooray!\n";
 
-  for (int i = 0; i < 3; i++)
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   std::cout << "Hip Hip hooray\n";
+  // }
+
+  // std::cout << isPrime(5);
+
+
+  //FILE Input/OUTPUT
+
+  int nextInt;
+  std::ifstream fin("ints.txt");
+  std::ofstream fout("primex.txt");
+
+  while (fin >> nextInt)
   {
-    std::cout << "Hip Hip hooray";
+    if (isPrime(nextInt)) 
+    {
+      fout << nextInt << std::endl;
+    }
   }
+  fout << std::endl;
+  fin.close();
+  fout.close();
+
+}
+
+bool isPrime(int n)
+{
+  for (int i=2;i<n/2;i++)
+  {
+    if (n%i==0)
+    {
+      return false;
+    }
+  }
+  return true;
 }
